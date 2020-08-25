f17d58d6965540448cdb8ee87db841b1ec274dd7
vinuch <vincentedeh42@gmail.com>
2020-08-24 18:08:19 -0700

    update api  url

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import axios from "axios"
 
 const AuthAxios = axios.create({
-  baseURL: 'http://127.0.0.1:5000/'
+  baseURL: process.env.NODE_ENV == 'development' ? 'http://127.0.0.1:5000/' : 'https://sony-casting-agency.herokuapp.com'
 })
 
 Vue.use(Vuex);
